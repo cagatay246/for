606a45d1ac4b595c4e92c0cf92a580baedc5fbbb
cagatay246 <cagataykilgi7@gmail.com>
2024-12-23 13:44:35 +0300

    for

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1->Konsol ekranına 10 kere "Kendime inanıyorum, ben bu yazılım işini hallederim!" yazdırınız.
+
+            //for (int i = 1; i <= 10; i ++)
+            //{
+            //    Console.WriteLine($"Kendime inanıyorum, ben bu yazılım işini hallederim!" + i);
+            //}
+
+            //2-> 1 ile 20 arasındaki sayıları konsol ekranına yazdırınız.
+
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine("döngü adımı:" + i );
+            //}
+            //3-> 1 ile 20 arasındaki çift sayıları konsol ekranına yazdırınız.
+
+            //for (int i = 1; i <= 20; i += 2)
+            //{
+            //    Console.WriteLine("çift sayılar");
+            //}
+
+            //4-> 50 ile 150 arasındaki sayıların toplamını ekrana yazdırınız.
+
+            //int toplam = 0;
+
+            
+            //for (int i = 50; i <= 150; i++)
+            //{
+            //    toplam += i;  
+            //}
+
+            //Console.WriteLine("50 ile 150 arasındaki sayıların toplamı: " + toplam);
+
+            //5-> 1 ile 120 arasındaki tek ve çift sayıların toplamlarını ayrı ayrı ekrana yazdırınız.
+
+            //int i = 0, 
+            //tekToplam = 0, 
+            //ciftToplam = 0;
+
+            //for (i = 1; i <= 120; i += 2)
+            //    tekToplam += i;
+
+            //for (i = 0; i <= 120; i += 2)
+            //    ciftToplam += i;
+            
+            //Console.WriteLine("Tek sayıların toplamı = " + tekToplam);
+            //Console.WriteLine("Çift sayıların toplamı = " + ciftToplam);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
